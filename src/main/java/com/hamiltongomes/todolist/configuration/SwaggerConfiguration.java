3e75c066ef5576b8a5a8f25f804d57a5d5dab57a
Hamilton <hamilton.gomes8@hotmail.com>
2025-03-09 22:41:27 -0300

    feat: update SwaggerConfiguration with improved API description, contact info and license

--- src/main/java/com/hamiltongomes/todolist/configuration/SwaggerConfiguration.java
+++ src/main/java/com/hamiltongomes/todolist/configuration/SwaggerConfiguration.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,12 +16,15 @@ public class SwaggerConfiguration {
         return new OpenAPI()
                 .components(new Components())
                 .info(new Info()
-                        .title("API todolist")
-                        .version("1.0")
-                        .description("Documentação da API do desafio backend da Simplify")
+                        .title("Todolist API")
+                        .version("1.0.0")
+                        .description("API de gerenciamento de tarefas, desenvolvida como parte do desafio backend Simplify. Permite criar, atualizar, listar e excluir tarefas.")
                         .contact(new Contact()
                                 .name("Hamilton Gomes")
                                 .email("hamilton.gomes8@hotmail.com")
-                                .url("https://github.com/hamiltonGomes/todolist-challenge-backend-simplify")));
+                                .url("https://github.com/hamiltonGomes/todolist-challenge-backend-simplify"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")));
     }
 }
